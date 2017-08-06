7085285f0d65cf09e2d3eba43c5dfd2262a07d04
Bart Langelaan <langelaanbart@gmail.com>
2017-08-06 23:01:09 +0200

    feat(site): add site class with tape test

--- src/Site.ts
+++ src/Site.ts
@@ -1,5 +1,42 @@
+// tslint:disable-next-line:import-name
+import fetch from 'universal-fetch';
+import deepmerge from 'deepmerge';
+
 class Site {
 
+  private url: string;
+
+  private data: object;
+
+  constructor({ url }: {url: string}) {
+
+    this.url = url;
+
+  }
+
+  fetch(options: object): Promise<object> {
+    return fetch({
+      method: 'GET',
+      mode: 'cors',
+      cache: 'default',
+      ...options,
+    }).then(result => result.json());
+  }
+
+  getPage({ path }: {path: string}) {
+    return this.fetch({ url: this.url + '/hn?_format=hn&path=' + encodeURIComponent(path) })
+      .then((page) => {
+        this.addData(page);
+      });
+  }
+
+  private addData(data: object) {
+    this.data = deepmerge(this.data, data, { arrayMerge: (a, b) => a.concat(b) });
+  }
+
+  getData() {
+    return this.data;
+  }
 }
 
 export default Site;
---
+++ tests/unit/site.js
@@ -0,0 +1,12 @@
+var test = require('tape');
+var hn = require('../../lib');
+
+test('creating a site', function(t){
+
+  t.plan(2);
+
+  var site = new hn.Site({ url: 'http://localhost:8088' });
+  t.ok(site instanceof hn.Site);
+  t.equal(typeof site.getPage, 'function');
+
+});
