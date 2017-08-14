8f4e750eb9ed8c99f154884623c71997a3aa3ae9
Ian Wensink <ian@burst-digital.com>
2017-08-14 16:29:16 +0200

    feat(hn): loads extra json initial state from window

--- src/site.js
+++ src/site.js
@@ -1,9 +1,14 @@
 import { Site } from 'hn';
+import getNested from 'get-nested';
 
-const site =  new Site({url: 'http://localhost:8088'});
+const site = new Site({ url: '' });
 
-if(typeof window !== 'undefined') {
+if(getNested(() => window.__NEXT_DATA__.HN_DATA)) {
   site.data = window.__NEXT_DATA__.HN_DATA;
+  site.url = window.__NEXT_DATA__.HN_URL || site.url;
+  site.pagesLoading = {
+    [window.location.pathname]: Promise.resolve(),
+  };
 }
 
 export default site;
