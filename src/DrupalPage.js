69a22dba8ca423159140ff2051f2f96c583f0986
Ian Wensink <ian@burst-digital.com>
2017-08-14 16:28:35 +0200

    feat(hn): adds mapper to map Drupal content types to components

--- src/DrupalPage.js
+++ src/DrupalPage.js
@@ -1,9 +1,11 @@
-import { Component } from 'react';
+import React, { Component } from 'react';
 import { parse } from 'url';
+import PropTypes from 'prop-types';
+import getNested from 'get-nested';
 import site from './site';
 
 export default class extends Component {
-  static async getInitialProps ({ asPath }) {
+  static async getInitialProps({ asPath }) {
     const location = parse(asPath, true);
     const page = await site.getPage(asPath, true);
 
@@ -11,5 +13,50 @@ export default class extends Component {
       location,
       page
     };
+  }
+
+  static propTypes = {
+    layout: PropTypes.oneOfType([
+      PropTypes.func,
+      PropTypes.string,
+    ]),
+    mapper: PropTypes.oneOfType([
+      PropTypes.shape(),
+      PropTypes.func,
+    ]).isRequired,
+  };
+
+  static defaultProps = {
+    layout: 'div',
+  };
+
+  render() {
+    const mapper = this.props.mapper;
+
+    const Layout = this.props.layout;
+
+    const page = site.getData(this.props.page);
+
+    if(typeof getNested(() => page.type.target_id) !== 'string') {
+      return null;
+    }
+
+    const ContentType = (typeof mapper === 'object') ? mapper[page.type.target_id] : mapper(page.type.target_id);
+
+    if(!ContentType) {
+      console.error('Component for content type', page.type.target_id, 'not found.');
+      return null;
+    }
+
+
+    return (
+      <Layout
+        page={page}
+        location={this.props.location}
+        history={this.props.history}
+      >
+        <ContentType page={page} />
+      </Layout>
+    );
   }
 }
