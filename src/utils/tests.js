9810fb2bc6cb6ae1140f7c031468178d415db39f
Bart Langelaan <langelaanbart@gmail.com>
2018-02-17 19:35:39 +0100

    test: add unit tests for Paragraph and Paragraphs components

---
+++ src/utils/tests.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+const mapperComponent = jest.fn((props) => {
+  return <div className="MyCustomMapperComponent" {...props} />
+});
+
+export const mapper = {
+  unique_type__unique_bundle: mapperComponent
+};
+
+export const uuid = 'unique-uuid';
+
+export const entity = {
+  __hn: {
+    entity: {
+      type: 'unique_type',
+      bundle: 'unique_bundle'
+    }
+  }
+};
+
+export function mockSite() {
+  class SiteMock {
+    getData = jest.fn(() => {
+      return entity;
+    });
+  }
+  return new SiteMock();
+}
