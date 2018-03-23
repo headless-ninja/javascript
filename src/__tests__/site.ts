4982a97e271c95fa4f1dc81566c90179914b9384
Bart Langelaan <langelaanbart@gmail.com>
2018-03-23 15:36:54 +0100

    build: replace nyc and tape with jest

---
+++ src/__tests__/site.ts
@@ -0,0 +1,26 @@
+import * as fetchMock from 'fetch-mock';
+import { Site } from '..';
+
+fetchMock.get(
+  'http://headless.ninja/tests/hn?_format=hn&path=%2Ftest123',
+  require('./response-1.json'), // tslint:disable-line:no-var-requires
+);
+
+test('creating a site', () => {
+  const site = new Site({ url: 'http://headless.ninja/tests' });
+  expect(site).toBeInstanceOf(Site);
+  expect(typeof site.getPage).toBe('function');
+});
+
+test('getting a page', () => {
+  const site = new Site({ url: 'http://headless.ninja/tests' });
+  site
+    .getPage('/test123')
+    .then(entityUUID => {
+      expect(entityUUID).toBe('e3842f1c-e148-46ae-a3e8-c03badbcb05b');
+      expect(site.getData(entityUUID).body.value).toBe('This is a body');
+    })
+    .catch(err => {
+      throw new Error(err);
+    });
+});
--- tests/support/server.js
+++
@@ -1,3 +0,0 @@
-var fetchMock = require('fetch-mock');
-
-fetchMock.get('http://headless.ninja/tests/hn?_format=hn&path=%2Ftest123', require('./response-1.json'));
