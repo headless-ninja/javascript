54e32f3032b2d860e9dd1b84bc0d6610da8d617a
Bart Langelaan <langelaanbart@gmail.com>
2018-05-04 15:19:09 +0200

    style: fix prettier errors

--- packages/hn/src/__tests__/site.ts
+++ packages/hn/src/__tests__/site.ts
@@ -3,17 +3,17 @@ import { Site } from '..';
 
 fetchMock.get(
   'http://headless.ninja/tests/hn?_format=hn&path=%2Ftest123',
-  async () => require('./response-1.json')
+  async () => require('./response-1.json'),
 );
 
 fetchMock.get(
   'http://headless.ninja/tests/hn?_format=hn&path=%2Ftranslations',
-  async () => require('./response-with-translations.json')
-)
+  async () => require('./response-with-translations.json'),
+);
 
 beforeEach(() => {
   fetchMock.reset();
-})
+});
 
 test('creating a site', () => {
   const site = new Site({ url: 'http://headless.ninja/tests' });
@@ -24,7 +24,7 @@ test('creating a site', () => {
 test('initializing a site', () => {
   const site = new Site();
   site.initialize({ url: 'http://headless.ninja/tests' });
-})
+});
 
 test('initializing a site twice', () => {
   const errorMessage = 'The site is already initialized.';
@@ -48,7 +48,8 @@ test('initializing a site twice', () => {
 });
 
 test('not initializing throws an error', () => {
-  const errorMessage = 'Site is not intitialized. Pass an object when creating a site, or use the initialize method.';
+  const errorMessage =
+    'Site is not intitialized. Pass an object when creating a site, or use the initialize method.';
 
   const site = new Site();
   expect(() => {
@@ -63,7 +64,7 @@ test('dehydrating and hydrating', async () => {
   const dehydration1 = site.dehydrate();
 
   // Create a new clean site with a different url.
-  const site2 = new Site({ url: 'http://this-shoudnt-be-called'});
+  const site2 = new Site({ url: 'http://this-shoudnt-be-called' });
 
   // Without a hydration, this will throw.
   await expect((async () => site2.getPage('/test123'))()).rejects.toBeTruthy();
@@ -93,13 +94,16 @@ test('getting a page', () => {
 });
 
 test('getting a page while its already loading', async () => {
-
   const site = new Site({ url: 'http://headless.ninja/tests' });
 
   let anyPromiseResolved = false;
 
-  const promise1 = site.getPage('/test123').then(() => anyPromiseResolved = true);
-  const promise2 = site.getPage('/test123').then(() => anyPromiseResolved = true);
+  const promise1 = site
+    .getPage('/test123')
+    .then(() => (anyPromiseResolved = true));
+  const promise2 = site
+    .getPage('/test123')
+    .then(() => (anyPromiseResolved = true));
 
   // Make sure promises aren't resolved yet.
   expect(anyPromiseResolved).toBe(false);
@@ -108,7 +112,7 @@ test('getting a page while its already loading', async () => {
   await Promise.all([promise1, promise2]);
 
   // There should only be one call made.
-  expect(fetchMock.calls(true as any as string).length).toBe(1);
+  expect(fetchMock.calls((true as any) as string).length).toBe(1);
 });
 
 test('getting a page twice', async () => {
@@ -118,7 +122,7 @@ test('getting a page twice', async () => {
   await site.getPage('/test123');
 
   // There should only be one call made
-  expect(fetchMock.calls(true as any as string).length).toBe(1);
+  expect(fetchMock.calls((true as any) as string).length).toBe(1);
 });
 
 test('translate function', async () => {
@@ -131,14 +135,18 @@ test('translate function', async () => {
 
   // The default langcode should be determined based on the langcode of the current entity.
   // The default is english.
-  history.pushState({}, "page 2", "/unrelated-path");
+  history.pushState({}, 'page 2', '/unrelated-path');
   expect(site.t('Text to translate')).toBe('Translate result');
 
   // If we change the path to a dutch entity, the same command should return a dutch string.
-  history.pushState({}, "page 2", "/entity-with-nl-langcode");
+  history.pushState({}, 'page 2', '/entity-with-nl-langcode');
   expect(site.t('Text to translate')).toBe('Dutch translate result');
 
   expect(site.t('Text without translation')).toBe('Text without translation');
-  expect(site.t('Text without translation', 'en')).toBe('Text without translation');
-  expect(site.t('Text without translation', 'nl')).toBe('Text without translation');
-})
+  expect(site.t('Text without translation', 'en')).toBe(
+    'Text without translation',
+  );
+  expect(site.t('Text without translation', 'nl')).toBe(
+    'Text without translation',
+  );
+});
